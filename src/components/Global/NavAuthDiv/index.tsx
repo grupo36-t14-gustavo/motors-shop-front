bc5ac1903a60e32e651ff3b35fa7dcc09df6ce9a
Bryan <bryanbatista79688@gmail.com>
2023-06-20 20:51:49 -0300

    style: Home component styles
    Imports Inter/lexend fonts from google fonts, change globalStyles and layoutGlobal scss files, modifies returnAxiosError api handler

--- src/components/Global/NavAuthDiv/index.tsx
+++ src/components/Global/NavAuthDiv/index.tsx
@@ -1,18 +1,19 @@
 import styles from "./style.module.scss";
 import RegisterRedirect from "../RegisterRedirectAnchor";
 import UserAvatar from "../UserAvatarDiv";
+import Link from "next/link";
 
 const NavAuth = () => {
     return (
         <div className={styles.nav_bar__auth_div}>
             {/* If User not signed: */}
-            <a className={styles.auth_div__login_redirect} href="">
+            <Link className={styles.auth_div__login_redirect} href="/login">
                 Fazer Login
-            </a>
+            </Link>
             <RegisterRedirect type="nav" />
 
             {/* If User IS signed */}
-            <UserAvatar />
+            {/* <UserAvatar /> */}
         </div>
     );
 };
