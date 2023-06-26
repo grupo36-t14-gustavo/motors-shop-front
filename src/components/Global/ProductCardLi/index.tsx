a150778441b09ea572b6bb9014e97fee20081de9
Bryan <bryanbatista79688@gmail.com>
2023-06-26 10:15:25 -0300

    style: Home banner and cardList component styles

--- src/components/Global/ProductCardLi/index.tsx
+++ src/components/Global/ProductCardLi/index.tsx
@@ -1,20 +1,29 @@
 import styles from "./style.module.scss";
 import UserContainer from "../UserContainerSpan";
 import TagContainer from "../TagContainerDiv";
+import globalFonts from "../../../styles/globalFonts.module.scss";
 
 const ProductCard = () => {
     return (
         <li className={styles.product_card}>
-            <img src="" alt="" />
-            <h3 className={styles.product_card__heading}>Porche 718</h3>
-            <p className={styles.product_card__description}>
+            <img className={styles.product_card__img} src="https://source.unsplash.com/random" alt="" />
+            <h3
+                className={`${styles.product_card__heading} ${globalFonts.heading_7_600}`}
+            >
+                Porche 718
+            </h3>
+            <p
+                className={`${styles.product_card__description} ${globalFonts.body_2_400}`}
+            >
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem...
             </p>
             <UserContainer />
             <div className={styles.product_card__tag_price_div}>
                 <TagContainer />
-                <span className={styles.tag_price_div__price_span}>
+                <span
+                    className={`${styles.tag_price_div__price_span} ${globalFonts.heading_7_500}`}
+                >
                     R$ 00.000,00
                 </span>
             </div>
