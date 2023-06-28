06942be13471fbf7d5e2217b832d4c0e4cab18a5
Mateus Rezende <mateusrezende1997@hotmail.com>
2023-06-28 15:32:44 -0300

    feat:added login service

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,28 +1,32 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { z } from "zod";
+import { string, z } from "zod";
 import Button from "../Global/Button";
 import Input from "../Global/Input/index";
 import InputPassword from "../Global/Input/input.Password";
 import Label from "../Global/Label/index";
 import styles from "./style.module.scss";
+import { userLoginRoute } from "@/services/api/User";
 const schemaLogin = z.object({
     email: z.string().email(),
     password: z.string(),
 });
 
 const LoginForm = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const data = {
             email: formData.get("emailInput"),
             password: formData.get("passwordInput"),
         };
         try {
-            schemaLogin.parse(data);
+            const payload = schemaLogin.parse(data);
             //colocar um tost pra indicar o usuario
             //colocar a requisição login
+            const token = await userLoginRoute(payload);
+            localStorage.setItem("token", token.token);
         } catch (error) {
             //tratar o erro com o toast
         }
